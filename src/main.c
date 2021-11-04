6b2f9e3fa1860b72aae4b08beb2090e7c9df640f
Lucas Ransan <lucas@ransan.tk>
2021-11-04 20:38:58 +0100

    add command line

--- src/main.c
+++ src/main.c
@@ -1,8 +1,11 @@
 #define _GNU_SOURCE
 
+#include <argp.h>
 #include <jansson.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "ast.h"
 #include "eval.h"
@@ -11,17 +14,84 @@
 #include "parse.h"
 #include "scan.h"
 
+#define IGNORE_MISSING_FIELD(X...)                                      \
+    _Pragma("GCC diagnostic push")                                      \
+    _Pragma("GCC diagnostic ignored \"-Wmissing-field-initializers\"")  \
+    X                                                                   \
+    _Pragma("GCC diagnostic pop")
+
 extern ASTProg *ast;
 
-int main(void) {
-    yyin = stdin;
+const char *argp_program_version = "pc 0.1.0";
+const char *argp_program_bug_address = "<lucas@ransan.tk>";
+
+static char doc[] = "pc -- pseudocode interpreter";
+static char args_doc[] = "[FILE]";
+
+IGNORE_MISSING_FIELD (
+static struct argp_option options[] = {
+    { "ast", 'a', 0, 0, "Dump abstract syntax tree" },
+    { 0 }
+};
+)
+
+struct args {
+    char *file;
+    bool ast;
+};
+
+static error_t parse_opt(int key, char *arg, struct argp_state *state) {
+    struct args *args = state->input;
+    switch (key) {
+    case 'a':
+        args->ast = true;
+        break;
+    case ARGP_KEY_ARG:
+        if (state->arg_num >= 1)
+            argp_usage(state);
+        args->file = arg;
+        break;
+    case ARGP_KEY_END:
+        break;
+    default:
+        return ARGP_ERR_UNKNOWN;
+    }
+    return 0;
+}
+
+IGNORE_MISSING_FIELD (
+static struct argp argp = { options, parse_opt, args_doc, doc };
+)
+
+int main(int argc, char **argv) {
+    struct args args = {
+        .file = "-",
+        .ast = false,
+    };
+
+    argp_parse(&argp, argc, argv, 0, 0, &args);
+
+    bool read_stdin = strcmp(args.file, "-") == 0;
+
+    FILE *f = read_stdin ? stdin : fopen(args.file, "r");
+    if (f == NULL) {
+        fprintf(stderr, "cannot access '%s': %s\n", args.file, strerror(errno));
+        return EXIT_FAILURE;
+    }
+
+    yyin = f;
     yyparse();
 
-    json_t *j = ast_json(ast);
-    //eval(ast);
+    if (args.ast) {
+        json_t *j = ast_json(ast);
+        json_dumpf(j, stdout, JSON_INDENT(2));
+        printf("\n");
+    } else {
+        eval(ast);
+    }
 
-    json_dumpf(j, stdout, JSON_INDENT(2));
-    puts("");
+    if (!read_stdin)
+        fclose(f);
 
-    return 0;
+    return EXIT_SUCCESS;
 }
